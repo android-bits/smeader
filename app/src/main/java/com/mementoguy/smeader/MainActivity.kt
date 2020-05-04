154309ff7acf46a4d4b4ed638a739d60267c1fd6
EdwardMuturi <kakaturi300@gmail.com>
2020-05-04 04:47:00 +0300

    delete fab and fragments, not using them

--- app/src/main/java/com/mementoguy/smeader/MainActivity.kt
+++ app/src/main/java/com/mementoguy/smeader/MainActivity.kt
@@ -30,10 +30,6 @@ class MainActivity : AppCompatActivity() {
 
         requestSmsPermission()
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
 
     }
 
